18e0c0bb98f655191bd52b628507da843c62837c
Jack Wimberley <jwimberley@paradigm4.com>
2023-05-10 14:35:23 +0000

    Adding a simple example of a DataFusion SQL query

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use rustyshim::SciDBConnection;
+use datafusion::prelude::*;
+use tokio; // 0.3.5
 
-use arrow::util::pretty::print_batches;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 
     // Config...
@@ -11,11 +12,14 @@ fn main() {
     let password = ""; // does not matter in trust mode
     let scidbport = 1239;
 
-    // Connect...
+    // Connect to SciDB...
     let mut conn = SciDBConnection::new(hostname, username, password, scidbport);
     if let SciDBConnection::Closed(status) = conn {
         panic!("Connection to SciDB failed! status code {status}");
     }
+
+    // Create a DataFusion context
+    let ctx = SessionContext::new();
 
     // Run a query...
     let query = "apply(build(<value:int64> [i=0:10:0:10;j=0:10:0:10],i*j),i,i,j,j)";
@@ -32,9 +36,27 @@ fn main() {
             );
             // at this point data is still on-disk in buffer file
             let data = aio.to_batches().unwrap(); // consumes buffer file, data lives in memory
-            for batch in data {
-                print_batches(&[batch]).unwrap();
+            let record_batch = arrow::compute::concat_batches(&data[0].schema(), &data).unwrap();
+            let reg = ctx.register_batch("example", record_batch);
+            match reg {
+                Err(error) => println!(
+                    "Error while loading batch into DataFusion:\n\n{}",
+                    error
+                ),
+                _ => ()
             }
+        }
+    }
+
+    // Run a DataFusion query...
+    let results = ctx.sql("SELECT value, MIN(i), MIN(j) FROM example GROUP BY value LIMIT 100").await;
+    match results {
+        Err(error) => println!(
+            "Error while running SQL via DataFusion:\n\n{}",
+            error
+        ),
+        Ok(df) => {
+            df.show().await;
         }
     }
 }
