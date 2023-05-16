60515aef96aaa923d8146d45ee15dc7a7355916d
Jack Wimberley <jwimberley@paradigm4.com>
2023-05-16 18:24:46 +0000

    Authenticate via SciDB, with an example implementation of a ClientAuthHandler for pyarrow client usage

---
+++ examples/pyarrow_auth_handler.py
@@ -0,0 +1,29 @@
+import pyarrow.flight as pf
+
+class RustyShimConnection:
+    class AuthHandler(pf.ClientAuthHandler):
+        def __init__(self, username, password):
+            self.username = username
+            self.password = password
+            self.token = None
+        def authenticate(self, outgoing, incoming):
+            outgoing.write(self.username)
+            outgoing.write(self.password)
+            self.token = incoming.read()
+        def get_token(self):
+            if self.token is None:
+                raise pf.FlightUnauthenticatedError("Not authenticated via SciDB")
+            return self.token
+    
+    def __init__(self, location, username, password):
+        self.client = pf.connect(location)
+        h = RustyShimConnection.AuthHandler(username, password)
+        self.client.authenticate(h)
+        self.options = pf.FlightCallOptions(headers=[(b'authorization',h.get_token())])
+    
+    def get_sql(self, query):
+        ticket = pf.Ticket(query)
+        return(self.client.do_get(ticket, self.options))
+
+def rustyshim_connect(location, username, password):
+    return(RustyShimConnection(location, username, password))
