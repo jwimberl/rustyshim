c5cfcec4a67ff95775ebf07e63102f22d2d3ccf6
Jack Wimberley <jwimberley@paradigm4.com>
2023-05-23 15:46:39 +0000

    Update R client; consider making an R6 wrapper instead of having verbose helper method names.

--- examples/rustyshim_client.R
+++ examples/rustyshim_client.R
@@ -1,12 +1,17 @@
-rustyshim_connect <- function(host = "localhost", username, password, port, scheme = "grpc+tcp") 
+rustyshim_connect <- function(host = "localhost", username, password, port, scheme = "grpc+tcp", request_admin = FALSE) 
 {
     prs <- reticulate::import("rustyshim_client")
     location <- paste0(scheme, "://", host, ":", port)
-    prs$rustyshim_connect(location,username, password)
+    prs$rustyshim_connect(location,username,password,request_admin=request_admin)
 }
 
 rustyshim_get_sql <- function(client, path) 
 {
     reader <- client$get_sql(path)
     reader$read_all()
+}
+
+rustyshim_refresh_context <- function(client) 
+{
+    client$refresh_context()
 }
